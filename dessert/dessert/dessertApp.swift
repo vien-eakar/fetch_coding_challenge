ac92c8400f866024218140a21fa30c1a42982960
vien <viennv@newbees.jp>
2024-07-01 14:15:51 -0400

    create new project

---
+++ dessert/dessert/dessertApp.swift
@@ -0,0 +1,17 @@
+//
+//  dessertApp.swift
+//  dessert
+//
+//  Created by Nguyen Vien on 7/1/24.
+//
+
+import SwiftUI
+
+@main
+struct dessertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
